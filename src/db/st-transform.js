ceff01cd9350c0f50b7a74008ec2cb56c50b4450
Dan O'Neill <dev@jdoneill.com>
2020-09-13 18:09:59 -0700

    rename st-transform

--- src/db/st-transform.js
+++ src/db/st-transform.js
@@ -1,11 +1,8 @@
-
 // st.asGeoJson(st.transform('geom', 4326))
-
 function asGeoJson(transform){
     this.rawType = true; // no escaping, because we return pre-formatted SQL
     this.toPostgres = () => pgp.as.format('ST_AsGeoJSON($1)', [transform]);
 }
-
 
 // st.transform('geom', 4326)
 function transform(column, proj){
