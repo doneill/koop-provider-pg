1bc3aa1436aa839f91d63353f484f6c51c8ef03b
Dan O'Neill <dev@jdoneill.com>
2020-09-20 20:50:10 -0700

    add simple popup

--- src/view/map.js
+++ src/view/map.js
@@ -1,7 +1,13 @@
 var map = L.map('map').setView([36.9591311, -121.97182], 10);
 
-L.esri.basemapLayer('Topographic').addTo(map);
+L.esri.basemapLayer('DarkGray', {
+detectRetina: true
+}).addTo(map);
 
-L.esri.featureLayer({
+var postgis = L.esri.featureLayer({
 url: 'http://localhost:8080/pg/rest/services/groot.fire_service_areas/FeatureServer/layers'
 }).addTo(map);
+
+postgis.bindPopup(function (layer) {
+    return L.Util.template('{name}', layer.feature.properties);
+  });
