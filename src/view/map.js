d2ba2a0cab494700cac7a2c1f9be4c34add571c3
Dan O'Neill <dev@jdoneill.com>
2020-10-03 16:28:52 +0000

    update formatting

--- src/view/map.js
+++ src/view/map.js
@@ -1,13 +1,13 @@
 var map = L.map('map').setView([36.9591311, -121.97182], 10);
 
 L.esri.basemapLayer('DarkGray', {
-detectRetina: true
+  detectRetina: true
 }).addTo(map);
 
 var postgis = L.esri.featureLayer({
-url: 'http://localhost:8080/pg/rest/services/groot.fire_service_areas/FeatureServer/layers'
+  url: 'http://localhost:8080/pg/rest/services/groot.fire_service_areas/FeatureServer/layers'
 }).addTo(map);
 
 postgis.bindPopup(function (layer) {
-    return L.Util.template('{name}', layer.feature.properties);
-  });
+  return L.Util.template('{name}', layer.feature.properties);
+});
