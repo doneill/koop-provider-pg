86d2d1143b2a161859813bd2dac5d56c70121fb9
Dan O'Neill <dev@jdoneill.com>
2020-11-21 10:43:10 -0800

    limit single geometry column table

--- src/db/sql/getGeometryColumnName.sql
+++ src/db/sql/getGeometryColumnName.sql
@@ -1,3 +1,4 @@
 SELECT f_geometry_column
   FROM geometry_columns
-  WHERE f_table_schema = $[schema] AND f_table_name = $[table];
+  WHERE f_table_schema = $[schema] AND f_table_name = $[table]
+  LIMIT 1;
