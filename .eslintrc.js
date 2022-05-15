2efc877b5ac2c6c41ffe48e0783f588ac5cabe89
Dan O'Neill <dev@jdoneill.com>
2022-05-15 15:46:30 -0700

    initial eslint setup

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    env: {
+        node: true,
+        jest:true,
+        es2021: true,
+    },
+    extends: [
+        'eslint:recommended',
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+    },
+    rules: {
+
+    },
+};
