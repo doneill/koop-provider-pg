e178519bd5b2b6539bf3f6edb3a20ee07665163f
Dan O'Neill <dev@jdoneill.com>
2024-05-04 11:06:11 -0700

    Add case clause if srid is not specified explicitly set it to 4326

--- src/db/sql/getGeometryColumnName.sql
+++ src/db/sql/getGeometryColumnName.sql
@@ -1,4 +1,5 @@
-SELECT f_geometry_column, srid
+SELECT f_geometry_column, 
+       CASE WHEN srid = 0 THEN 4326 ELSE srid END AS srid 
   FROM geometry_columns
   WHERE f_table_schema = $[schema] AND f_table_name = $[table]
   LIMIT 1;
